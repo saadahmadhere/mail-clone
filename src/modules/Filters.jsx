579887d9dad9c3d9e5c5576983a9cff2456026ee
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-06-09 23:02:42 +0530

    make basic filter layout

--- src/modules/Filters.jsx
+++ src/modules/Filters.jsx
@@ -1,7 +1,18 @@
 import '../styles/filter.css';
 
 const Filters = () => {
-	return <aside className='filter-container'>Filters</aside>;
+	return (
+		<aside className='filter-container'>
+			<span className='hey'></span>
+			<h2>Filters</h2>
+			<ul>
+				<li>inbox</li>
+				<li>draft</li>
+				<li>spam</li>
+				<li>trash</li>
+			</ul>
+		</aside>
+	);
 };
 
 export default Filters;
